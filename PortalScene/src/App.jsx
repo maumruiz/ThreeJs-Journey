e5be131e73efb710a73fadd1f4f8896b3d6df2c6
maumruiz <mau.mruiz@gmail.com>
2023-07-12 18:38:08 -0600

    Add chromatic glitch effect

--- PortalScene/src/App.jsx
+++ PortalScene/src/App.jsx
@@ -2,15 +2,16 @@ import { Suspense } from 'react'
 import { Perf } from 'r3f-perf'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { Bloom, EffectComposer, Vignette, ChromaticAberration } from '@react-three/postprocessing'
 
 import Portal from './Portal'
 import Fireflies from './Fireflies'
 import LoadbarSound from './Audio'
 import { LoadScene } from './LoadScene'
 import EnvMap from './EnvMap'
+import Effects from './Effects'
 
 export default function App() {
+
     return (
         <>
             <LoadScene />
@@ -22,13 +23,9 @@ export default function App() {
                     <Fireflies count={30} />
                     <Portal />
                 </Suspense>
-                <OrbitControls enablePan={false} autoRotate autoRotateSpeed={0.2} 
-                                minPolarAngle={0} maxPolarAngle={Math.PI / 2} maxDistance={15} />
-                <EffectComposer enabled={true} multisampling={0}>
-                    <Bloom mipmapBlur luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-                    <Vignette offset={0.3} darkness={0.5} />
-                    <ChromaticAberration offset={[0.002, 0.002]} />
-                </EffectComposer>
+                <OrbitControls enablePan={false} autoRotate autoRotateSpeed={0.2}
+                    minPolarAngle={0} maxPolarAngle={Math.PI / 2} maxDistance={15} />
+                <Effects />
             </Canvas>
             <LoadbarSound />
         </>
---
+++ PortalScene/src/Effects.jsx
@@ -0,0 +1,57 @@
+import { useRef } from 'react'
+import { Bloom, EffectComposer, Vignette, ChromaticAberration } from '@react-three/postprocessing'
+import { useFrame } from '@react-three/fiber'
+import { Vector2, Vector3 } from "three";
+
+function randomFloat(low, high) {
+	return low + Math.random() * (high - low);
+}
+
+let time = 0
+let trigger = false
+
+export default function Effects() {
+    const chromaticEffect = useRef()
+
+    const delay = new Vector2(2.0, 4.5)
+	const duration = new Vector2(0.2, 0.7)
+    const strength = new Vector2(0.002, 0.009)
+    let breakpoint = new Vector3(
+        randomFloat(delay.x, delay.y),
+        randomFloat(duration.x, duration.y),
+        randomFloat(strength.x, strength.y)
+    )
+
+    useFrame((state, delta) => {
+        time += delta
+        trigger = time > breakpoint.x
+
+        if (time > breakpoint.x + breakpoint.y) {
+            breakpoint.set(
+                randomFloat(delay.x, delay.y),
+                randomFloat(duration.x, duration.y),
+                randomFloat(strength.x, strength.y)
+            )
+            time = 0
+        }
+
+        if(trigger) {
+            const offsetTime = time - breakpoint.x
+            const offset = breakpoint.z * Math.sin(offsetTime * (2*Math.PI*(1/breakpoint.y)))
+            chromaticEffect.current.offset.x = offset
+            chromaticEffect.current.offset.y = offset
+        }
+        else {
+            chromaticEffect.current.offset.x = 0
+            chromaticEffect.current.offset.y = 0
+        }
+    })
+
+    return (
+        <EffectComposer enabled={true} multisampling={0}>
+            <Bloom mipmapBlur luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+            <Vignette offset={0.3} darkness={0.5} />
+            <ChromaticAberration ref={chromaticEffect} offset={[0.002, 0.002]} />
+        </EffectComposer>
+    )
+}
